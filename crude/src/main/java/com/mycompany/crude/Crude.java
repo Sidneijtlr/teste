f6e0c000c60db696647d3d70ad2f374d4889993c
Sidneijtlr <sidneiwilpertjunior2016@gmail.com>
2024-05-27 20:54:32 -0300

    Add files via upload

---
+++ crude/src/main/java/com/mycompany/crude/Crude.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crude;
+
+/**
+ *
+ * @author 122323266
+ */
+public class Crude {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
